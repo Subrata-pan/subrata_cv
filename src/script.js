9d42c1942a870f18029bbdd2b9fb56c421cdc006
Subrata-pan <simapan1996@gmail.com>
2025-03-19 08:01:42 +0530

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.work',{
+    strings:['Web Developer', 'Web Designer','Software Engineer','UI/UX Designer','freelancer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backdelay: 1000,
+    loop:true,
+})
